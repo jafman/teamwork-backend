f3977906fdcdbb391a71323e9ffa33f284c0b911
Abduljafar Kamarudeen <abduljafar.kamaru@ekedp.com>
2019-11-23 13:58:15 +0100

    used-promise-in-controller

--- controller/employee.js
+++ controller/employee.js
@@ -1,8 +1,29 @@
 const employee = require('../db/employee');
 
+// eslint-disable-next-line no-unused-vars
 exports.createEmployee = (req, res, next) => {
-  const fullName = req.body.fullName;
-  const email = req.body.email;
+  const userFullName = req.body.fullName;
+  const userEmail = req.body.email;
   const password = req.body.password;
-  employee.createEmployee(fullName, email, password, req, res);
+  employee.createEmployee(userFullName, userEmail, password)
+    .then(
+      ({ fullName, email }) => {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            fullName,
+            email
+          }
+        });
+      }
+    )
+    .catch(
+      (error) => {
+        console.log(error.stack);
+        res.status(500).json({
+          status: 'error',
+          error: error.detail
+        });
+      }
+    );
 };
--- db/employee.js
+++ db/employee.js
@@ -1,30 +1,23 @@
 const pool = require('./db');
 
 
-exports.createEmployee = (fullname, email, password, request, response) => {
-  pool.connect((error, client, done) => {
-    if (error) throw error;
-    const queryString = 'INSERT INTO employee (full_name, email, password, created_on) VALUES ($1,$2,$3,NOW())';
-    const valueString = [fullname, email, password];
+exports.createEmployee = (fullName, email, password) => {
+  return new Promise((resolve, reject) => {
+    pool.connect((error, client, done) => {
+      if (error) throw error;
+      const queryString = 'INSERT INTO employee (full_name, email, password, created_on) VALUES ($1,$2,$3,NOW())';
+      const valueString = [fullName, email, password];
 
-    client.query(queryString, valueString, (err, res) => {
-      done();
-      if (err) {
-        console.log(err.stack);
-        response.status(500).json({
-          status: 'error',
-          error: err.detail
-        });
-      } else {
-        // console.log(res.rows[0]);
-        response.status(201).json({
-          status: 'success',
-          data: {
-            fullname,
-            email
-          }
-        });
-      }
+      // eslint-disable-next-line no-unused-vars
+      client.query(queryString, valueString, (err, res) => {
+        done();
+        if (err) {
+          reject(err);
+        } else {
+          // console.log(res.rows[0]);
+          resolve({ fullName, email });
+        }
+      });
     });
   });
 };
